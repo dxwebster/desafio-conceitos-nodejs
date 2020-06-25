3df2726bef0867d5d44c6dc25694f3b32310db9d
Adriana Lima <adrianalimafm@gmail.com>
2020-06-24 21:50:09 -0300

    Projects CRUD, Likes

--- src/__tests__/likes.spec.js
+++ src/__tests__/likes.spec.js
@@ -3,7 +3,7 @@ const app = require("../app");
 
 describe("Likes", () => {
   it("should be able to give a like to the repository", async () => {
-    const repository = await request(app)
+    const repository = await request(app) // cria um repositorio com uma requisição post
       .post("/repositories")
       .send({
         url: "https://github.com/Rocketseat/umbriel",
@@ -11,11 +11,11 @@ describe("Likes", () => {
         techs: ["Node", "Express", "TypeScript"]
       });
 
-    let response = await request(app).post(
+    let response = await request(app).post( // chama a rota de like 
       `/repositories/${repository.body.id}/like`
     );
 
-    expect(response.body).toMatchObject({
+    expect(response.body).toMatchObject({ // espera que seja pelo menos igual a 1
       likes: 1
     });
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,80 @@ app.use(cors());
 
 const repositories = [];
 
+//  Lista todos os repositórios
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
+//  Criação de repositório
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body; // recebe do corpo da requisição o title, url e techs
+
+  const repository = { // objeto do novo projeto
+    id:uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository); // armazena o novo projeto no repositorio
+  return response.json(repository); // retorna o objeto novo criado  em json
+
 });
 
+// Atualização de repositorio
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params; // importa o id vindo no parametro da rota
+  const {title, url, techs} = request.body; // importa as informações do body
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id); //pesquisa nos repositorios qual a posição do id que to recebendo
+
+  if (repositoryIndex < 0){ // se não encontrar, ou seja, o índice for abaixo de zero
+    return response.status(400).send({error: 'Repository not found'}) // retorna com status 400
+  }
+
+  // se encontrar, crio as atualizações do repositório, com as informações vinda do corpo
+  const repository = { 
+      id,
+      title,
+      url,
+      techs,
+      likes: repositories[repositoryIndex].likes, // no array repositorios eu procuro a mesma posição do respositório com id indicado, e recebo a quantidade de likes
+  };
+   
+    repositories[repositoryIndex] = repository;  // no array repositorios eu procuro a mesma posição do respositório com id indicado e atribuo as modificações    
+    return response.json(repository);// retorno o projeto atualizado
+
 });
 
+// deleta um repositorio
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params; // importa o id vindo no parametro da rota
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);//pesquisa nos repositorios qual a posição do id que to recebendo
+
+  if (repositoryIndex < 0){ // se não encontrar, ou seja, o índice for abaixo de zero
+      return response.status(400).json({error: 'Repository not found'}) // retorna com status 400
+  }
+
+  repositories.splice(repositoryIndex, 1); // caso encontre, remova a informação do índice repositoryIndex
+  return response.status(204).send(); // retorna em branco, geralmente envia com o status 204
 });
 
+
+// Permite dar like em um repositório
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params; // importa o id vindo no parametro da rota
+  const repository = repositories.find(repository => repository.id == id); //pesquisa nos repositorios se existe algum com o id que to recebendo
+
+ // Se não existir o repositorio, retorna status 400
+  if(!repository){
+   return response.status(400).send();
+ }
+ 
+  repository.likes += 1; // soma mais 1 no repositorio encontrado
+  return response.json(repository); // retorna o repositorio que recebeu o like
+
 });
 
 module.exports = app;
